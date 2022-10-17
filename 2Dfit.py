74f5a9968001e3d3c3c1b70fa601b38039196d64
felixxyang <kaifeng@ucsb.edu>
2022-10-17 15:17:56 -0700

    files rename

--- 2Dfit.py
+++ 2Dfit.py
@@ -1,31 +1,52 @@
 """
-data fitting on RSM from Rigaku XRD
+data fitting on 2Theta v.s. Intensity curve from Rigaku XRD
 """
 
-import numpy as np
-from scipy.optimize import curve_fit
+import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy import optimize
+import scipy.integrate as integrate
+import math
 import click
+import sys
+import json
 
-def gaussian(x, y, x0, y0, xalpha, yalpha, A):
-    return A * np.exp( -((x-x0)/xalpha)**2 -((y-y0)/yalpha)**2)
+def _1Voigt(x, ampG1, cenG1, sigmaG1, cenL1, widL1):
+    return (ampG1*(1/(sigmaG1*(np.sqrt(2*np.pi))))*(np.exp(-((x-cenG1)**2)/(2*(sigmaG1)**2)))) +\
+              (((1-ampG1)*widL1**2/(math.pi*((x-cenL1)**2+widL1**2))) )  
 
-def lorentzian(x, y, x0, y0, w, A):
-    return A * (w**2) / ((x-x0)**2 + (y-y0)**2 + (w)**2)
-
-def voigt(x, y, x0, y0, xalpha, yalpha, Ag, w, Al):
-    return gaussian(x, y, x0, y0, xalpha, yalpha, Ag) + lorentzian(x, y, x0, y0, w, Al)
-
-def _voigt(M, *args):
-    x, y = M
-    arr = np.zeros(x.shape)
-    arr += voigt(x, y, *args[0:7])
-    return arr
- 
 def find_nearest(array, value):
     array = np.asarray(array)
     idx = (np.abs(array - value)).argmin()
-    return idx
+    return array[idx]
+
+def FWHM(X,Y):
+    half_max = np.max(Y) / 2
+    max_index = np.argmax(Y)
+    left_nearest = find_nearest(Y[:max_index], half_max)
+    right_nearest = find_nearest(Y[max_index:], half_max)
+    left_index = np.where(Y[:max_index] == left_nearest)
+    right_index = np.where(Y[max_index:] == right_nearest) + max_index
+    return X[right_index[0][0]]-X[left_index[0][0]]
+
+def Rsquared(Y,Yfit):
+    ss_res = np.sum((Y - Yfit)**2)
+    ss_tot = np.sum((Y - np.mean(Y))**2)
+    R2 = 1 - (ss_res / ss_tot)
+    return R2
+
+def AdjRsquared(Y,Yfit,pars):
+    ss_res = np.sum((Y - Yfit)**2)
+    ss_tot = np.sum((Y - np.mean(Y))**2)
+    n = len(Y)
+    K = len(pars)
+    AR2 = 1 - ((ss_res/ (n - K)  ) / (ss_tot/ (n - 1)  ))
+    return AR2
+
+def integration(df, pars):
+    result = integrate.quad(lambda x:_1Voigt(x, *pars), df['angle'].iat[0], df['angle'].iat[-1])
+    return result[0]
 
 @click.command()
 @click.option(
@@ -33,133 +54,84 @@ def find_nearest(array, value):
     "-input",
     prompt=True,
     help=(
-        "RMS txt file from Rigaku"
+        "Intensity vs 2Theta txt file from Rigaku"
     )
 )
-def handle_input(input_file):
-    #create empty list for Omega, 2theta and Intensity
-    w = []
-    t2 = []
-    I = []
+@click.option(
+    "--output-file",
+    "-output",
+    prompt=True,
+    help=(
+        "Output file for data of this measurement"
+    )
+)
 
-    # Build array of lines from file, strip newlines
-    mylines = []                                                    # Declare an empty list.
-    with open (input_file, 'rt') as myfile:    # Open txt doc for reading text.
-        for myline in myfile:                                       # For each line in the file,
-            mylines.append(myline.rstrip('\n'))                     # strip newline and add to list.
-
-    #Extract w
-    for line in mylines:
-        if line[:26] == "*MEAS_COND_AXIS_POSITION-0":
-            w.append(float(line[28:-1]))
-
-    #Locate all lines' indexes above all the t2 and I data sections
-    lineAbove = []
-    index = 0
-    for line in mylines:
-        if line == "#Attenuator_coefficient=1.0000":
-            lineAbove.append(index)
-        index += 1
-
-    #Find the index of the line below the first t2 and I data section and use that to calculate the length of each data section
-    firstLineBelow = mylines[1:].index("*FILE_COMMENT \"\"")
-    t2Length = firstLineBelow - lineAbove[0] 
-
-    #Extract t2
-    for line in mylines[lineAbove[0]+1 : firstLineBelow+1]:
-        i = line.index(" ")
-        t2.append(float(line[:i]))
-        
-    #Extract I - 2D Array
-    for lineIndex in lineAbove:
-        Ii = []
-        for line in mylines[lineIndex + 1 : lineIndex + t2Length + 1]:
-            i = line.index(" ")
-            Ii.append(float(line[i+1:]))
-        I.append(Ii)
-
-    #Reorder w and I, because w are not organized in order in the text file 
-    w, I = zip(*sorted(zip(w, I)))
-
-    #Convert the data lists into arrays
-    y = np.asarray(w)
-    x = np.asarray(t2)
-    Z = np.asarray(I)
-
-    # The two-dimensional domain of the fit.
-    X, Y = np.meshgrid(x, y)
+def handle_input(input_file, output_file):
     
-    # Plot the 3D figure of the fitted function and the residuals.
-    fig = plt.figure()
-    ax = fig.add_subplot(projection='3d')
-    ax.plot_surface(X, Y, Z, cmap='plasma')
-    ax.set_zlim(0,np.max(Z)+2)
-    plt.show()
+    if sys.platform == "win32":
+        sampleName = input_file.split("\\")[-1].split("_")[1]
+        planeName = input_file.split("\\")[-1].split("_")[2].split(".")[0]
+    else:
+        sampleName = input_file.split("/")[-1].split("_")[1]
+        planeName = input_file.split("/")[-1].split("_")[2].split(".")[0]
     
-    # Initial guesses to the fit parameters. 
-    xFWHM = abs(x[np.where(Z == np.max(Z))[1][0]] - 
-                x[find_nearest(Z[np.where(Z == np.max(Z))[0][0]], np.max(Z)/2)])
-    xSigma = xFWHM / 2.355
-
-    curve_y = []
-
-    for i in Z:
-        curve_y.append(i[np.where(Z == np.max(Z))[1][0]])
-
-    yFWHM = abs(y[np.where(Z == np.max(Z))[0][0]] - 
-                y[find_nearest(curve_y, np.max(Z)/2)])
-    ySigma = yFWHM / 2.355
-
-    guess_prms = [x[int(len(x)/2)],y[int(len(y)/2)],xSigma,ySigma,np.max(Z)/2,((xFWHM+yFWHM)/2),np.max(Z)/2]
-
-    # We need to ravel the meshgrids of X, Y points to a pair of 1-D arrays.
-    xdata = np.vstack((X.ravel(), Y.ravel()))
-
-    # Do the fit, using our custom _gaussian function which understands our
-    # flattened (ravelled) ordering of the data points.
-    popt, pcov = curve_fit(_voigt, xdata, Z.ravel(), p0=guess_prms, maxfev=100000)
-    fit = np.zeros(Z.shape)
-    fit += voigt(X, Y, *popt[0:7])
-    print('Fitted parameters:')
-    print(popt)
+    df = pd.read_csv(input_file, delim_whitespace=True, header=0, names=['angle','intensity'], skiprows = 777)
+    fig1, ax1 = plt.subplots()
+    ax1.plot(df['angle'],df['intensity'],label="measurement")
+    ax1.set_xlabel('2$\Theta$')
+    ax1.set_ylabel('intensity')
+    ax1.set_title('data & fitting')
+    #fit
+    max_position = df['angle'][df['intensity'][df['intensity'] == np.max(df['intensity'])].index[0]]
+    wid = FWHM(df['angle'], df['intensity'])
+    #sigma = FWHM(df['angle'], df['intensity'])/2.355 
+    #Guassian is turned off in Voigt to get better fit
+    guess_prms = [0, 1, 1, max_position, wid]
+    pars, cov = optimize.curve_fit(f=_1Voigt, xdata=df['angle'], ydata=df['intensity'],p0=guess_prms)
+    ax1.plot(df['angle'], _1Voigt(df['angle'], *pars), label="fitted curve")
+    ax1.legend()
+    #plt.show()
     
-    #RMS residual
-    rms = np.sqrt(np.mean((Z - fit)**2))
-    print('RMS residual =', rms)
-
-    # Plot the 3D figure of the fitted function and the residuals.
-    fig = plt.figure()
-    ax = fig.add_subplot(projection='3d')
-    ax.plot_surface(X, Y, fit, cmap='plasma')
-    cset = ax.contourf(X, Y, Z-fit, zdir='z', offset=-4, cmap='plasma')
-    ax.set_zlim(-4,np.max(fit))
-    plt.show()
-
-    # Plot the 3D figure of the residuals
-    fig = plt.figure()
-    ax = fig.add_subplot(projection='3d')
-    ax.plot_surface(X, Y, Z-fit, cmap='plasma')
-    plt.show()
-
-    # Plot the test data as a 2D image and the fit as overlaid contours.
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.imshow(Z, origin='lower', cmap='plasma',
-              extent=(x.min(), x.max(), y.min(), y.max()))
-    ax.contour(X, Y, fit, colors='w')
-    plt.show()
-
-    print('Peak location is at 2theta = ', popt[0] , 'and w = ' , popt[1] )
-
-
-
+    #residula
+    fig2, ax2 = plt.subplots()
+    ax2.plot(df['angle'], df['intensity']-_1Voigt(df['angle'], *pars))
+    ax2.set_title('residual')
+    ax2.set_xlabel('2$\Theta$')
+    ax2.set_ylabel('difference in intensity')
+    #plt.show()
+    
+    area = integration(df, pars)
+    print("The arae under the peak is", area)
+    fwhm = FWHM(df['angle'], _1Voigt(df['angle'], *pars))
+    print("The FWHM is", fwhm)
+    R2 = Rsquared( df['intensity'], _1Voigt(df['angle'], *pars))
+    print("The coefficient of determination, R^2, is", R2)
+    AdjustedR2 = AdjRsquared( df['intensity'], _1Voigt(df['angle'], *pars), pars)
+    print("The adjusted R^2 is", AdjustedR2)
+    theta = pars[3] / 2
+    print("Peak postition is located at", 2 * theta)
+    #Rigaku uses Cu as X-ray source
+    d = 1.5406 / (2 * np.sin(theta))
+    print("d-space is", d)
+    
+    #create dictionary
+    data_dict = {}
+    data_dict[sampleName+"_"+planeName] = {
+        "d" : d,
+        "area" : area,
+        "fwhm" : fwhm,
+        "R2" : R2,
+        "AdjustedR2": AdjustedR2
+        }
+    
+    with open(output_file, "w") as ofs:
+        json.dump(data_dict, ofs, indent=2)
+    
     
 if __name__ == "__main__":
     handle_input()
 
+    
+    
 
-
-
-
-
-
+    
---
+++ RSMfit.py
@@ -0,0 +1,165 @@
+"""
+data fitting on RSM from Rigaku XRD
+"""
+
+import numpy as np
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
+import click
+
+def gaussian(x, y, x0, y0, xalpha, yalpha, A):
+    return A * np.exp( -((x-x0)/xalpha)**2 -((y-y0)/yalpha)**2)
+
+def lorentzian(x, y, x0, y0, w, A):
+    return A * (w**2) / ((x-x0)**2 + (y-y0)**2 + (w)**2)
+
+def voigt(x, y, x0, y0, xalpha, yalpha, Ag, w, Al):
+    return gaussian(x, y, x0, y0, xalpha, yalpha, Ag) + lorentzian(x, y, x0, y0, w, Al)
+
+def _voigt(M, *args):
+    x, y = M
+    arr = np.zeros(x.shape)
+    arr += voigt(x, y, *args[0:7])
+    return arr
+ 
+def find_nearest(array, value):
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return idx
+
+@click.command()
+@click.option(
+    "--input-file",
+    "-input",
+    prompt=True,
+    help=(
+        "RMS txt file from Rigaku"
+    )
+)
+def handle_input(input_file):
+    #create empty list for Omega, 2theta and Intensity
+    w = []
+    t2 = []
+    I = []
+
+    # Build array of lines from file, strip newlines
+    mylines = []                                                    # Declare an empty list.
+    with open (input_file, 'rt') as myfile:    # Open txt doc for reading text.
+        for myline in myfile:                                       # For each line in the file,
+            mylines.append(myline.rstrip('\n'))                     # strip newline and add to list.
+
+    #Extract w
+    for line in mylines:
+        if line[:26] == "*MEAS_COND_AXIS_POSITION-0":
+            w.append(float(line[28:-1]))
+
+    #Locate all lines' indexes above all the t2 and I data sections
+    lineAbove = []
+    index = 0
+    for line in mylines:
+        if line == "#Attenuator_coefficient=1.0000":
+            lineAbove.append(index)
+        index += 1
+
+    #Find the index of the line below the first t2 and I data section and use that to calculate the length of each data section
+    firstLineBelow = mylines[1:].index("*FILE_COMMENT \"\"")
+    t2Length = firstLineBelow - lineAbove[0] 
+
+    #Extract t2
+    for line in mylines[lineAbove[0]+1 : firstLineBelow+1]:
+        i = line.index(" ")
+        t2.append(float(line[:i]))
+        
+    #Extract I - 2D Array
+    for lineIndex in lineAbove:
+        Ii = []
+        for line in mylines[lineIndex + 1 : lineIndex + t2Length + 1]:
+            i = line.index(" ")
+            Ii.append(float(line[i+1:]))
+        I.append(Ii)
+
+    #Reorder w and I, because w are not organized in order in the text file 
+    w, I = zip(*sorted(zip(w, I)))
+
+    #Convert the data lists into arrays
+    y = np.asarray(w)
+    x = np.asarray(t2)
+    Z = np.asarray(I)
+
+    # The two-dimensional domain of the fit.
+    X, Y = np.meshgrid(x, y)
+    
+    # Plot the 3D figure of the fitted function and the residuals.
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    ax.plot_surface(X, Y, Z, cmap='plasma')
+    ax.set_zlim(0,np.max(Z)+2)
+    plt.show()
+    
+    # Initial guesses to the fit parameters. 
+    xFWHM = abs(x[np.where(Z == np.max(Z))[1][0]] - 
+                x[find_nearest(Z[np.where(Z == np.max(Z))[0][0]], np.max(Z)/2)])
+    xSigma = xFWHM / 2.355
+
+    curve_y = []
+
+    for i in Z:
+        curve_y.append(i[np.where(Z == np.max(Z))[1][0]])
+
+    yFWHM = abs(y[np.where(Z == np.max(Z))[0][0]] - 
+                y[find_nearest(curve_y, np.max(Z)/2)])
+    ySigma = yFWHM / 2.355
+
+    guess_prms = [x[int(len(x)/2)],y[int(len(y)/2)],xSigma,ySigma,np.max(Z)/2,((xFWHM+yFWHM)/2),np.max(Z)/2]
+
+    # We need to ravel the meshgrids of X, Y points to a pair of 1-D arrays.
+    xdata = np.vstack((X.ravel(), Y.ravel()))
+
+    # Do the fit, using our custom _gaussian function which understands our
+    # flattened (ravelled) ordering of the data points.
+    popt, pcov = curve_fit(_voigt, xdata, Z.ravel(), p0=guess_prms, maxfev=100000)
+    fit = np.zeros(Z.shape)
+    fit += voigt(X, Y, *popt[0:7])
+    print('Fitted parameters:')
+    print(popt)
+    
+    #RMS residual
+    rms = np.sqrt(np.mean((Z - fit)**2))
+    print('RMS residual =', rms)
+
+    # Plot the 3D figure of the fitted function and the residuals.
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    ax.plot_surface(X, Y, fit, cmap='plasma')
+    cset = ax.contourf(X, Y, Z-fit, zdir='z', offset=-4, cmap='plasma')
+    ax.set_zlim(-4,np.max(fit))
+    plt.show()
+
+    # Plot the 3D figure of the residuals
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    ax.plot_surface(X, Y, Z-fit, cmap='plasma')
+    plt.show()
+
+    # Plot the test data as a 2D image and the fit as overlaid contours.
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.imshow(Z, origin='lower', cmap='plasma',
+              extent=(x.min(), x.max(), y.min(), y.max()))
+    ax.contour(X, Y, fit, colors='w')
+    plt.show()
+
+    print('Peak location is at 2theta = ', popt[0] , 'and w = ' , popt[1] )
+
+
+
+    
+if __name__ == "__main__":
+    handle_input()
+
+
+
+
+
+
+

bd6176084fb66174ea89442d38098ffdadc118c6
felixxyang <kaifeng@ucsb.edu>
2022-11-21 13:31:10 -0800

    subtracting noise

--- 2Dfit.py
+++ 2Dfit.py
@@ -11,10 +11,14 @@ import math
 import click
 import sys
 import json
+import statistics
 
 def _1Voigt(x, ampG1, cenG1, sigmaG1, cenL1, widL1):
     return (ampG1*(1/(sigmaG1*(np.sqrt(2*np.pi))))*(np.exp(-((x-cenG1)**2)/(2*(sigmaG1)**2)))) +\
               (((1-ampG1)*widL1**2/(math.pi*((x-cenL1)**2+widL1**2))) )  
+
+def linearFit(x, k, b):
+    return k * x + b
 
 def find_nearest(array, value):
     array = np.asarray(array)
@@ -44,9 +48,30 @@ def AdjRsquared(Y,Yfit,pars):
     AR2 = 1 - ((ss_res/ (n - K)  ) / (ss_tot/ (n - 1)  ))
     return AR2
 
-def integration(df, pars):
-    result = integrate.quad(lambda x:_1Voigt(x, *pars), df['angle'].iat[0], df['angle'].iat[-1])
-    return result[0]
+def integration(df, pars, fwhm):
+
+    angle_arr = np.asarray(df['angle'])
+    int_arr = np.asarray(df['intensity'])
+    peak = find_nearest( angle_arr , pars[3])
+    peak_ind = np.where(angle_arr == peak)[0][0]
+    left_cutoff = find_nearest( angle_arr[:peak_ind] , peak - 3*fwhm)
+    right_cutoff = find_nearest( angle_arr[peak_ind:] , peak + 3*fwhm)
+    lc_index = np.where(angle_arr[:peak_ind] == left_cutoff)[0][0]
+    rc_index = np.where(angle_arr[peak_ind:] == right_cutoff)[0][0] + peak_ind
+    
+    pars_l, cov_l = optimize.curve_fit(f=linearFit, xdata=angle_arr[:lc_index], ydata=int_arr[:lc_index])
+    pars_r, cov_r = optimize.curve_fit(f=linearFit, xdata=angle_arr[rc_index:], ydata=int_arr[rc_index:])
+    ave_k = (pars_l[0] + pars_r[0]) / 2
+    ave_b = (pars_l[1] + pars_r[1]) / 2
+    
+    for i in range(len(angle_arr)):
+        int_arr[i] -= linearFit(angle_arr[i], ave_k, ave_b)  
+        
+    area = np.trapz(int_arr, angle_arr)
+    
+    return area, peak_ind, lc_index, rc_index, angle_arr, int_arr
+    #result = integrate.quad(lambda x:_1Voigt(x, *pars), df['angle'].iat[0], df['angle'].iat[-1])
+    #return result[0]
 
 #spicify local xlsx file (sample growth temperatures) location here 
 def findTemp(sample):
@@ -97,21 +122,32 @@ def handle_input(input_file, output_file):
     guess_prms = [0, 1, 1, max_position, wid]
     pars, cov = optimize.curve_fit(f=_1Voigt, xdata=df['angle'], ydata=df['intensity'],p0=guess_prms)
     ax1.plot(df['angle'], _1Voigt(df['angle'], *pars), label="fitted curve")
-    ax1.legend()
+    #ax1.legend()
     #plt.show()
     
     #residula
-    fig2, ax2 = plt.subplots()
-    ax2.plot(df['angle'], df['intensity']-_1Voigt(df['angle'], *pars))
-    ax2.set_title('residual')
-    ax2.set_xlabel('2$\Theta$')
-    ax2.set_ylabel('difference in intensity')
+    #fig2, ax2 = plt.subplots()
+    #ax2.plot(df['angle'], df['intensity']-_1Voigt(df['angle'], *pars))
+    #ax2.set_title('residual')
+    #ax2.set_xlabel('2$\Theta$')
+    #ax2.set_ylabel('difference in intensity')
     #plt.show()
     
-    area = integration(df, pars)
-    print("The arae under the peak is", area)
+    
     fwhm = FWHM(df['angle'], _1Voigt(df['angle'], *pars))
     print("The FWHM is", fwhm)
+    
+    area, p, lci, rci, new_angle, new_int = integration(df, pars, fwhm)
+    print("The arae under the peak is", area)
+    ax1.axvline(df['angle'][lci], color='r')
+    ax1.axvline(df['angle'][rci], color='r')
+    ax1.axhline(0, color='r')
+    #ax1.axvline(df['angle'][p], color='orange')
+    ax1.plot(new_angle, new_int, label='background cut-out')
+    ax1.legend()
+    plt.show()
+    
+    
     R2 = Rsquared( df['intensity'], _1Voigt(df['angle'], *pars))
     print("The coefficient of determination, R^2, is", R2)
     AdjustedR2 = AdjRsquared( df['intensity'], _1Voigt(df['angle'], *pars), pars)
@@ -142,11 +178,9 @@ def handle_input(input_file, output_file):
     
     with open(output_file, "w") as ofs:
         json.dump(data_dict, ofs, indent=2)
-    
-    
+       
 if __name__ == "__main__":
     handle_input()
-
     
     
 
